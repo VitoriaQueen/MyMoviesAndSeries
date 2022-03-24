fb57b7c000a34ece0bdfcb09341652bba36ff461
Vitoria Oliveira <viklic@gmail.com>
2022-03-23 23:19:44 -0400

    Add project files.

---
+++ My-Movies-Database/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Movies_Database.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
